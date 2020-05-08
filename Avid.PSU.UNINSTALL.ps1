7a6b3c355b31aff1e37e2d175b19daf535f84219
Karol Flont <karol.flont@avid.com>
2020-05-08 15:10:59 +0200

    Release uninstall script

---
+++ Avid.PSU.UNINSTALL.ps1
@@ -0,0 +1,24 @@
+﻿Write-Host -ForegroundColor Green "`nUninstalling Avid.PSUtilities module... "
+
+# destination - using user PS module path
+$PSMPaths = $env:PSModulePath -split ';'
+foreach ($PSMPAth in $PSMPaths) {
+  if ($PSMPath -like "*Documents*"){
+    $destination = $PSMPath
+  }
+}
+
+Remove-Item -LiteralPath ($destination + "\" + "Avid.PSUtilities") -Recurse -Force
+Write-Host -ForegroundColor Green "`nModule Avid.PSUtilites uninstalled. "
+
+### Unloading the module from the active memory
+Remove-Module Avid.PSUtilities -Force
+Write-Host -ForegroundColor Green "`nModule Avid.PSUtilites unloaded from the active memory. "
+
+### Clearing "all hosts" from trusted hosts
+Set-Item WSMan:\localhost\Client\TrustedHosts -Value "" -Force
+Write-Host -ForegroundColor Green "`nWSMan:\localhost\Client\TrustedHosts cleared. "
+
+### Footer message
+Write-Host -ForegroundColor Green "Hit enter to close this window. "
+[void](Read-Host)
