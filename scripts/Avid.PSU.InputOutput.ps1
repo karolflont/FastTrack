6d67caf2fc360c5457e53aedc49f708e8fe1cb3a
Karol Flont <karol.flont@avid.com>
2020-05-08 15:08:57 +0200

    add aliases support for Import-AvSystemConfiguration function

--- scripts/Avid.PSU.InputOutput.ps1
+++ scripts/Avid.PSU.InputOutput.ps1
@@ -16,9 +16,9 @@ function Import-AvSystemConfiguration{
 
     Write-Host -ForegroundColor Green "`nLists of hosts created based on the system configuration file: "
 
-    #set All variable including all the servers, no matter if a server has any roles defined
-    $global:All = $SysConfigVar.hosts.IP
-    Write-Host "`$All = $global:All"
+    #set all variable including all the servers, no matter if a server has any roles defined
+    $global:all = $SysConfigVar.hosts.IP
+    Write-Host "`$all = $global:all"
 
     #define a list of unique roles
     $roles = $SysConfigVar.hosts.role | Sort-Object | Get-Unique
@@ -26,14 +26,31 @@ function Import-AvSystemConfiguration{
     #define role variables
     foreach ($role in $roles) {
         $roleServers = ($SysConfigVar.hosts | Where-Object {$_.role -Like $role}).IP
-        Set-Variable -Name "All$role" -Value $roleServers -Scope "global"
-        $varName = Get-Variable "All$role" | Select-Object -ExpandProperty Name
-        $varValue = Get-Variable "All$role" -ValueOnly
+        Set-Variable -Name "all$role" -Value $roleServers -Scope "global"
+        $varName = Get-Variable "all$role" | Select-Object -ExpandProperty Name
+        $varValue = Get-Variable "all$role" -ValueOnly
         Write-Host "`$$varName = $varVAlue"
     }
 
-    Write-Host -ForegroundColor Green "`nThe whole System Configuration is kept in json format in `$SysConfig variable. "
+    #define a list of aliases
+    $aliases = $SysConfigVar.hosts.alias | Sort-Object
 
+    #check is aliases are unique
+    if ($aliases -eq ($aliases | Get-Unique)){
+       #define alises variables
+        foreach ($alias in $aliases) {
+            $IPforAParticularAlias = ($SysConfigVar.hosts | Where-Object {$_.alias -Like $alias}).IP
+            Set-Variable -Name "$alias" -Value $IPforAParticularAlias -Scope "global"
+            $varName = Get-Variable "$alias" | Select-Object -ExpandProperty Name
+            $varValue = Get-Variable "$alias" -ValueOnly
+            Write-Host "`$$varName = $varValue"
+        }
+        Write-Host -ForegroundColor Green "`nThe whole System Configuration is kept in json format in `$SysConfig variable. "
+    }
+    else {
+        Write-Host -ForegroundColor Red "`nAliases used for hosts defined in the $path are not unique. Please modify your configuration file. "
+        return
+    }
 }
 
 function New-AvMRemoteNGSessionsConfiguration{}
