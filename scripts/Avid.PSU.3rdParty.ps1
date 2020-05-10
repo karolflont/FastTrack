a4543a6058bcb2181c1a8630a3ef7c34836d4e78
Karol Flont <karol.flont@avid.com>
2020-05-10 23:55:47 +0200

    Add Invoke-AvCMDExpression function

---
+++ scripts/Avid.PSU.3rdParty.ps1
@@ -0,0 +1,45 @@
+function Invoke-AvCMDExpression{
+   <#
+   .SYNOPSIS
+   Outputs the results of any given Windows Commandline (CMD) expression for a list of computers.
+   .DESCRIPTION
+   The Invoke-AvCMDExpression function uses:
+   - Invoke-Expression cmdlet
+   .PARAMETER ComputerIP
+   Specifies computer IP.
+   .PARAMETER Credentials
+   Specifies credentials used to login.
+   .PARAMETER CMDExpression
+   Specifies the CMD expression to run on remote computers.
+   .EXAMPLE
+   Invoke-AVCMDExpression -ComputerIP $all -Credential $cred -CMDExpression 'w32tm /query /status'
+   #>
+
+    Param(
+        [Parameter(Mandatory = $true)] $ComputerIP,
+        [Parameter(Mandatory = $true)] [System.Management.Automation.PSCredential] $Credential,
+        [Parameter(Mandatory = $true)] $CMDExpression
+    )
+
+    # Run command
+    $Result = Invoke-Command -ComputerName $ComputerName -Credential $Credential -ScriptBlock {
+        $result = Invoke-Expression $using:CMDExpression
+        [pscustomobject]@{CMDExpressionValue=$result}
+    }
+
+    # Import configuration variable
+    $sc = $global:SysConfig | ConvertFrom-Json
+
+    # Format and print results for each computer
+    foreach ($ResultLine in $Result){
+        Write-Host -ForegroundColor Green "`n-----------------------------------------------------------"
+        $CurrentIP = [string]$ResultLine.PSComputerName
+        $RemoteHostName = ($sc.hosts | Where-object {$_.IP -eq $CurrentIP}).hostname
+        $RemoteHostAlias = ($sc.hosts | Where-object {$_.IP -eq $CurrentIP}).alias
+        Write-Host "Alias / Hostname: $RemoteHostAlias / $RemoteHostName"
+        Write-Host "CMD Expression: $CMDExpression"
+        Write-Host -ForegroundColor Green "-----------------------------------------------------------"
+        $ResultLine.CMDExpressionValue
+        Write-Host -ForegroundColor Green "-----------------------------------------------------------"
+    }
+}
